6a724b4e28ebd0320c55b5cb35e839ed2130a3e0
Islam Bahnas <islambahnas1@gmail.com>
2022-05-30 16:50:34 +0200

    Add (add) methods for each class

--- item.rb
+++ item.rb
@@ -1,14 +1,36 @@
 class Item
   attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_reader :id, :archived
 
   def initialize(date, archived: false)
-    @id = rand(1..1000)
+    @id = rand(1...1000)
     @publish_date = date
     @archived = archived
+
+    @genre = []
+    @source = []
+    @author = []
+    @label = []
+  end
+
+  def add_genre(genre)
+    @genre << genre
+  end
+
+  def add_source(source)
+    @source << source
+  end
+
+  def add_author(author)
+    @author << author
+  end
+
+  def add_label(label)
+    @label << label
   end
 
   def can_be_archived?
-    @publish_date > 10
+    (Time.now.year - @publish_date.year) > 10
   end
 
   def move_to_archive
