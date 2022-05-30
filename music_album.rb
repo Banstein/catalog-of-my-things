7b4b15b099559e0b767c894f0288140a82ef5b96
unknown <okorojiebube2@gmail.com>
2022-05-30 15:49:39 -0600

    Modify music album class

--- music_album.rb
+++ music_album.rb
@@ -3,8 +3,12 @@ require './item'
 attr_accessor :on_spotify
 
 class MusicAlbum < Item
-  def initialize(on_spotify: true)
+  def initialize(on_spotify: false)
     super()
     @on_spotify = on_spotify
+  end
+
+  def can_be_archieved?(item)
+    item.can_be_archieved? && on_spotify = true
   end
 end
