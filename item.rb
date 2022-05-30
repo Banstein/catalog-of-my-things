30b860fa0bf196ae3542a9f472e6cd9fb8685c2f
unknown <okorojiebube2@gmail.com>
2022-05-30 08:29:32 -0600

    Add Item methods

--- item.rb
+++ item.rb
@@ -1,10 +1,18 @@
 class Item
 
-attr_accessor :genre, :author, :source, :label
+attr_accessor :genre, :author, :source, :label, :publish_date
 
   def initialize(date, archived: false)
     @id = rand(1..1000)
     @publish_date = date
     @archived = archived
+  end
+
+  def can_be_archived?
+    @publish_date > 10
+  end
+
+  def move_to_archive
+    @archived = true if self.can_be_archived?
   end
 end
