e565beb452c50d73d2d755727a1d55946a30df8e
BonkeG <madixmicro@gmail.com>
2022-05-31 12:33:12 +0200

    Write test for author

---
+++ specs/author_spec.rb
@@ -0,0 +1,12 @@
+require './author.rb'
+require '.game.rb'
+
+describe Author do
+  context "Test add_item method" do
+    it "Test if we can add item in author" do
+      author = Author.new(first_name, last_name)
+      item_string = author.add_item("A string")
+      expect(author.items.length()).to eq(1)
+    end
+  end
+end
