eba9794c61f209344d9489ed35479ce27b4ece6e
BonkeG <madixmicro@gmail.com>
2022-05-31 12:47:17 +0200

    Test if can add object of type game

--- specs/author_spec.rb
+++ specs/author_spec.rb
@@ -1,4 +1,5 @@
 require './author.rb'
+require './game.rb'
 
 describe Author do
   context "Test add_item method" do
@@ -6,6 +7,13 @@ describe Author do
       author = Author.new("Bonke", "Gcobo")
       item_string = author.add_item("A string")
       expect(author.items.length()).to eq(1)
+    end
+
+    it "Test if we can add item of class game in author" do
+      author = Author.new("Bonke", "Gcobo")
+      game = Game.new('12-12-1999', true, true, '12-12-2019')
+      author.add_item(game)
+      expect(author.items[0]).to be_a Game
     end
   end
 end
