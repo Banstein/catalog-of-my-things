a849d959c2728fceee321141dcb303b48761346f
BonkeG <madixmicro@gmail.com>
2022-05-31 12:15:35 +0200

    Add more test for game

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -2,9 +2,19 @@ require './game.rb'
 
 describe Game do
   context 'Test can_be_archived method in Game class' do  
-  game = Game.new('12-12-1999', true, true, '12-12-2019')
     it "Test if it can return true if game can be archived" do
-     expect(game.can_be_archived?).to be(false)
+      game = Game.new('12-12-1999', true, true, '12-12-2019')
+      expect(game.can_be_archived?).to be(true)
     end
-  end 
+
+    it "Test if it returns false if parent cannot be archived" do
+      game = Game.new('12-12-2020', true, true, '12-12-2021')
+      expect(game.can_be_archived?).to be(false)
+    end
+
+    it "Test if it returns false if child cannot be archived" do
+      game = Game.new('12-12-1995', true, true, '12-12-2021')
+      expect(game.can_be_archived?).to be(false)
+    end
+  end
 end
