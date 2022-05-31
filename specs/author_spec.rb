ce5b2726178870a057fb2c2e6e3ced0d6562ac1a
BonkeG <madixmicro@gmail.com>
2022-05-31 15:53:48 +0200

    update author test association

--- specs/author_spec.rb
+++ specs/author_spec.rb
@@ -1,19 +1,14 @@
 require './author.rb'
 require './game.rb'
+require './item'
 
 describe Author do
   context "Test add_item method" do
-    it "Test if we can add item in author" do
+    it "Test if the author of item is the given author" do
       author = Author.new("Bonke", "Gcobo")
-      item_string = author.add_item("A string")
-      expect(author.items.length()).to eq(1)
-    end
-
-    it "Test if we can add item of class game in author" do
-      author = Author.new("Bonke", "Gcobo")
-      game = Game.new('12-12-1999', true, true, '12-12-2019')
+      game = Item.new("12-12-2020")
       author.add_item(game)
-      expect(author.items[0]).to be_a Game
+      expect(author.items[0].author).to eq(author)
     end
   end
 end
